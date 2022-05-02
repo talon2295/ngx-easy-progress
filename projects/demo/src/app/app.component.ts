df7be3f3e991e54e387555574aa1206c375d098d
talon <gianluca.orsini@endotics.onmicrosoft.com>
2022-05-02 13:07:42 +0200

    added animation

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -27,19 +27,37 @@ export class AppComponent {
     fgColor: "#1144ff",
     thickness: "8px",
     borderRadius: "0",
+    animate: true,
+    animationParams: ".9s linear"
   }
   pbOptions3: progressBarOptions = {
     vertical: false,
     invert: true,
-    align: 'center'
+    animate: true
   }
   pbOptions4: progressBarOptions = {
     vertical: false,
     invert: false,
-    align: 'center'
+    animate: true
   }
+  pbOptions5: progressBarOptions = {
+    vertical: false,
+    invert: false,
+  }
+
+  mProgr: number = 0;
 
   constructor() {
     
+  }
+
+  get progr() {
+    return this.mProgr / 100 ;
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.mProgr = (this.mProgr + 10) % 100;
+    }, 1000)
   }
 }
--- projects/ngx-easy-progress/src/lib/ngx-easy-progress.component.ts
+++ projects/ngx-easy-progress/src/lib/ngx-easy-progress.component.ts
@@ -3,11 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export interface progressBarOptions {
   vertical?: boolean;
   invert?: boolean;
+  mode?: "progress" | "indeterminate",
   thickness?: string;
   fgColor?: string;
   bgColor?: string;
   borderRadius?: string;
   align?: "left" | "center" | "right";
+  animate?: boolean;
+  animationParams?: string;
 }
 
 @Component({
@@ -17,12 +20,20 @@ export interface progressBarOptions {
 })
 export class NgxEasyProgressComponent implements OnInit {
 
+  private defBorderRadius:string = "8px" ;
+  private detThickness:string = "20px" ;
+
   mProgress: number = 0 ; 
+  mBuffer: number = 0 ; 
 
   @Input("options") mOptions: progressBarOptions = {};
   @Input("progress") set progress(p:number) {
     let _p = p*100 ;
     this.mProgress = _p > 100 ? 100 : (_p < 0 ? 0 : _p) ;
+  }
+  @Input("buffer") set buffer(p:number) {
+    let _p = p*100 ;
+    this.mBuffer = _p > 100 ? 100 : (_p < 0 ? 0 : _p) ;
   }
   
   constructor() {
@@ -31,23 +42,33 @@ export class NgxEasyProgressComponent implements OnInit {
   get barStyle() {
     let _base:any = {
       'background-color': this.mOptions.fgColor || "#4477ee",
-      'border-radius': this.mOptions.borderRadius || "8px",
+      'border-radius': this.mOptions.borderRadius || this.defBorderRadius,
     } ;
 
     if(this.mOptions.vertical) {
       _base['height'] = (this.mProgress || 0) + "%" ;
+      if(this.mOptions.animate) {
+        _base['transition'] = "height " + (this.mOptions.animationParams || ".3s ease" );
+      }
     }
     else {
       _base['width'] = (this.mProgress || 0) + "%" ;
+      if(this.mOptions.animate) {
+        _base['transition'] = "width " + (this.mOptions.animationParams || ".3s ease" );
+      }
     }
 
     return _base ;
   }
 
+  get bufStyle() {
+    return {};
+  }
+
   get outerStyle() {
     let _base:any = {
       'background-color': this.mOptions.bgColor || "#ddd",
-      'border-radius': this.mOptions.borderRadius || "8px",
+      'border-radius': this.mOptions.borderRadius || this.defBorderRadius,
     } ;
 
     switch(this.mOptions.align) {
@@ -61,10 +82,10 @@ export class NgxEasyProgressComponent implements OnInit {
     }
 
     if(this.mOptions.vertical) {
-      _base['width'] = this.mOptions.thickness || "20px" ;
+      _base['width'] = this.mOptions.thickness || this.detThickness ;
     }
     else {
-      _base['height'] = this.mOptions.thickness || "20px" ;
+      _base['height'] = this.mOptions.thickness || this.detThickness ;
     }
     
     return _base ;
